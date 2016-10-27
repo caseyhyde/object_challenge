1140addade0c7146f4af31c8ffc516921faab857
Casey Hyde <hyde.casey@gmail.com>
2016-10-27 08:46:32 -0500

    This is the completed assignment

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+function State (state, revenue, locationCount) {
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+  this.averageRevenue = function() {
+      return this.revenue/this.locationCount;
+  };
+};
 // object instances
+var illinois = new State("IL", 5000, 12);
+var minnesota = new State("MN", 300, 3);
+var nevada = new State("NV", 25000, 1);
 
 // push object instances to locations array
+locations.push(illinois, minnesota, nevada);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
